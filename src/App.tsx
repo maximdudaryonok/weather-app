3e2b71ed4c5f73c0e4ca47653de4fa955ce9cf62
Maxim <maxim.dudaryonok@yandex.ru>
2025-12-16 19:23:02 +0200

    init: setup weather app with Vite and shadcn/ui

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./components/context/ThemeProvider";
+import WeatherDashboard from "./components/pages/WeatherDashboard";
+import CityPage from "./components/pages/CityPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>
+          <Routes>
+            <Route path="/" element={<WeatherDashboard />} />
+            <Route path="/city/:cityName" element={<CityPage />} />
+          </Routes>
+        </Layout>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
