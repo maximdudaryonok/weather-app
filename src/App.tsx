a081b7935539552e1a10937520d978cac2b97a1a
Maxim <maxim.dudaryonok@yandex.ru>
2025-12-30 23:12:38 +0200

    feat: add city search functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+// import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./components/layout";
@@ -7,7 +7,7 @@ import WeatherDashboard from "./components/pages/WeatherDashboard";
 import CityPage from "./components/pages/CityPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+// to create diferent route, we use react router as it is multipage application
 function App() {
   const queryClient = new QueryClient({
     defaultOptions: {
